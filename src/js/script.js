9ecce6a515cf8f1aff89c1bd8ae6319897e10fd1
ALEX <sifcrem@mail.ru>
2022-09-29 20:35:31 +0300

    dz1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+
+const numberOfFilms = +prompt ('Сколько фильмов вы уже смотрели', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один из последних просмотренных фильмов?', ''),
+    b = prompt ('Насколько оцените его?', ''),
+    c = prompt ('Один из последних просмотренных фильмов?', ''),
+    d = prompt ('Насколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
